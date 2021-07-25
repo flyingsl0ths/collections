e0eb5bc9002043c0dc2abe55c71e44dc72cbe3d1
flyingsl0ths <randomemail32110@gmail.com>
2021-07-25 09:55:45 -0500

    Moved file into 'array list/'

---
+++ examples/array list/int_array_list.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "../../src/collections/array_list/array_list.h"
+
+ARRAY_LIST_HEADER(int_list, int)
+
+comparison_result_t int_comparison(const int* const left, const int* const right);
